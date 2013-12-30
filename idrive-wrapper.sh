4a121b16c1f1b7b7dd6df120a6003dd996c4dd66
Nitin Mathew <nitn_mathew2000@hotmail.com>
2013-12-30 22:48:32 +0800

    Added the function to read config and init params
    Added the function to assign initial params, then read a config file and
    then assign the additional params. If config does not exist then the
    program will exit
    
    nitn_mathew2000@hotmail.com

--- idrive-wrapper.sh
+++ idrive-wrapper.sh
@@ -1,13 +1,66 @@
-#!/usr/bin/bash
-#Utility to backup local data to IDrive's cloud backup
-#Based on IDrive's command line utility, idevsutil
-#Copyright for IDrive's utility idevsutil is with IDrive, http://evs.idrive.com/terms-of-service.htm
-#Copyright for rest of the script Nitin Mathew, 2013
+#!/usr/bin/bash - 
+#===============================================================================
+#
+#          FILE: idrive-wrapper.sh
+# 
+#         USAGE: ./idrive-wrapper.sh 
+# 
+#   DESCRIPTION: Utility to backup local data to IDrive's cloud backup
+# 
+#        AUTHOR: Nitin Mathew, nitn.mathew2000@hotmail.com
+#       CREATED: 30/12/2013 21:19
+#     COPYRIGHT: Copyright for IDrive's utility idevsutil is with IDrive, http://evs.idrive.com/terms-of-service.htm
+#		 Copyright for rest of the script Nitin Mathew, 2013
+#===============================================================================
 
-FILELIST_UPLOAD=/home/nsm09/scripts/resources/FILELIST-IDRIVE-UPLOAD
-FILELIST_DELETE=/home/nsm09/scripts/resources/FILELIST-IDRIVE-DELETE
-USERID=nsmathew@gmail.com
-DESTFOLDER=ArchLinux
+set -o nounset                              # Treat unset variables as an error
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  read_config_init
+#   DESCRIPTION:  Read the config file and initialize the variables
+#    PARAMETERS:  
+#       RETURNS:  
+#-------------------------------------------------------------------------------
+read_config_init(){
+	#initial params
+	CONFIG=~/.config/.idrivewrc
+	WORKDIR=`mktemp -d`
+	echo ${WORKDIR}
+	
+	#If config file doesn exist then exit
+	if  [ ! -s "${CONFIG}" ]; then
+		echo "ERROR: Config file, ${CONFIG} does not exist. Exiting"
+		rm -fr ${WORKDIR}
+		exit 1
+	fi
+	
+	echo "INFO: Using config file ${CONFIG}"
+	#Populate the params from config file
+	USERID=`grep 'UNAME'  "${CONFIG}" | cut -d'=' -f 2`
+	echo "INFO: User ID is ${USERID}"
+	DESTFOLDER=`grep 'IDRIVE_HOME_FOLDER'  "${CONFIG}" | cut -d'=' -f 2`
+	echo "INFO: Parent folder in IDrive is ${DESTFOLDER}"
+	FILELIST_UPLOAD=${WORKDIR}/${USERID}_UPLOAD
+	FILELIST_DELETE=${WORKDIR}/${USERID}-DELETE
+	grep 'UPLOAD_LIST' "${CONFIG}" | cut -d'=' -f 2 | tr ';' '\n' | while read PTH; do
+		echo "${PTH}" >> ${FILELIST_UPLOAD}
+	done
+	PTH=""
+	grep 'DELETE_LIST' "${CONFIG}" | cut -d'=' -f 2 | tr ';' '\n' | while read PTH; do
+		echo "${PTH}" >> ${FILELIST_DELETE}
+	done
+}
+
+#Start Here
+echo "IDrive backup wrapper script."
+echo $(date +"%Y%m%d-%H%M%S")          # generate timestamp : YYYYMMDD-hhmmss
+
+#Read the config file and assign params and populate file lists
+read_config_init
+
+
+
+exit 1
 
 #Check if required variable are defined, exclude deletion list from check
 if [ ! -f ${FILELIST_UPLOAD} ] ||  [ "${USERID}" = "" ] || [ "${DESTFOLDER}" = "" ]
